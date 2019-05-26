7e27558c81a8cfd754cdd3a9edf51c7b643b0b78
jorcalle11 <jorcalle11@gmail.com>
2019-05-26 18:28:18 -0500

    The N+1 query problem, see the logs in the terminal

--- name-contests/data/index.js
+++ name-contests/data/index.js
@@ -4,20 +4,57 @@ const humps = require('humps');
 
 module.exports = function main(dbConnection) {
   function getUserByApiKey(key) {
-    return dbConnection
+    const query = dbConnection
       .select('*')
       .from('users')
-      .where('api_key', key)
-      .then(rows => humps.camelizeKeys(rows.shift()));
+      .where('api_key', key);
+
+    console.log(query.toQuery());
+    return query.then(rows => humps.camelizeKeys(rows.shift()));
+  }
+
+  function getUserById(userId) {
+    const query = dbConnection
+      .select('*')
+      .from('users')
+      .where('id', userId);
+
+    console.log(query.toQuery());
+    return query.then(rows => humps.camelizeKeys(rows.shift()));
   }
 
   function getContestsByUserId(userId) {
-    return dbConnection
+    const query = dbConnection
       .select('*')
       .from('contests')
-      .where('created_by', userId)
-      .map(row => humps.camelizeKeys(row));
+      .where('created_by', userId);
+
+    console.log(query.toQuery());
+    return query.map(row => humps.camelizeKeys(row));
   }
 
-  return { getUserByApiKey, getContestsByUserId };
+  function getNames(contestId) {
+    const query = dbConnection
+      .select('*')
+      .from('names')
+      .where('contest_id', contestId);
+
+    console.log(query.toQuery());
+    return query.map(row => humps.camelizeKeys(row));
+  }
+
+  function getCountsByUserId(userId, fieldName) {
+    return dbConnection
+      .collection('users')
+      .findOne({ userId })
+      .then(userCount => userCount[fieldName]);
+  }
+
+  return {
+    getUserByApiKey,
+    getContestsByUserId,
+    getCountsByUserId,
+    getNames,
+    getUserById
+  };
 };
